0764cca25f95ae09cd33c12d42ed38c2e1560194
Kavindra Sarma <Kavindra Sarma@Kavindra-Sharma>
2019-06-28 10:03:40 +0530

    second

---
+++ fundamentals/src/fundamentals/Ibex.java
@@ -0,0 +1,9 @@
+package fundamentals;
+
+public class Ibex
+	{
+		public static void main(String args[])
+		{
+			System.out.println(args[0]+" Technologies "+args[1]);
+		}
+	}
---
+++ fundamentals/src/fundamentals/Ibex2.java
@@ -0,0 +1,9 @@
+package fundamentals;
+
+public class Ibex2 {
+		public static void main(String args[])
+		{
+			System.out.println("Welcome "+args[0]);
+		}
+	}
+
